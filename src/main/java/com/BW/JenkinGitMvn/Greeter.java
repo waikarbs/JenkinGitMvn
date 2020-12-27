8ae3d4b58d3ec1e4168ca5a136a974d5e899de80
Waikar, Bharat <waikarbs@yahoo.com>
2020-12-27 09:32:17 +0530

    First move to local repository

---
+++ src/main/java/com/BW/JenkinGitMvn/Greeter.java
@@ -0,0 +1,13 @@
+package com.BW.JenkinGitMvn;
+
+
+public class Greeter 
+{
+	public Greeter() {
+		
+	}
+    public String greet(String someone) {
+    	return String.format("hello, %s!",  someone);
+    	
+    }
+}
